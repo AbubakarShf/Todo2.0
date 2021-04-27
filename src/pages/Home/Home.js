9379d2c2f2e14114e610e259f83c424a307e1054
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-04-28 02:37:59 +0500

    DesignPhase-Todo2.0

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { HomePage } from "./Home.element";
+const Home = () => {
+    return (
+        <React.Fragment>
+            <HomePage>
+                <h1>HomePage</h1>
+            </HomePage>
+        </React.Fragment>
+    );
+};
+
+export default Home;
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+    Container,
+    Heading,
+    FormContainer,
+    FormGroup,
+    SubmitButton,
+} from "./Login.element";
+const Login = () => {
+    return (
+        <Container>
+            <Heading>Login Here!</Heading>
+            <FormContainer>
+                <form action="">
+                    <FormGroup>
+                        <label htmlFor="name">Enter Full Name</label> <br />
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder="Full Name"
+                            required
+                        />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <label htmlFor="password">Enter Password</label> <br />
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="Password"
+                            required
+                        />
+                    </FormGroup>
+                    <SubmitButton type="submit">Submit</SubmitButton>
+                </form>
+            </FormContainer>
+        </Container>
+    );
+};
+
+export default Login;
